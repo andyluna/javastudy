c5e72fd9b8879f8ec910bb97fe2975884afd7eba
andyluna <xiangdan311@163.com>
2020-09-29 10:09:06 +0800

    java学习基础项目

---
+++ src/main/java/com/andy/study/jvm/stack/StackTest.java
@@ -0,0 +1,29 @@
+package com.andy.study.jvm.stack;
+
+/**
+ * TODO:
+ *
+ * @time: 2020/9/29 九月 10:07
+ * @author: xiangdan/xiangdan@dtxytech.com
+ */
+public class StackTest {
+
+    public static void main(String[] args) {
+        StackTest st = new StackTest();
+        st.method1();
+    }
+
+    private void method1(){
+        int a = 1;
+        int b = 1;
+
+        method2();
+    }
+
+    private void method2(){
+        int a = 1;
+        int b = 1;
+    }
+
+
+}
