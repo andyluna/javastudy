1d2c298c3b8a321ef275e83c4f43989edd84357b
xiangdan <xiangdan311@163.com>
2020-10-05 00:08:07 +0800

    直接内存

---
+++ src/main/java/com/andy/study/jvm/str/StringNewTest.java
@@ -0,0 +1,21 @@
+package com.andy.study.jvm.str;
+
+/**
+ * @TODO: javastudy
+ * @author: xiangdan/xiangdan@dtxytech.com
+ * @time: 2020/10/4 17:08  星期日
+ */
+public class StringNewTest {
+
+    public static void main(String[] args) {
+        String str = new String("abc");
+    }
+
+
+    public static void test1(){
+        String s1 = new String("a")+new String("b");
+
+
+
+    }
+}
---
+++ src/main/java/com/andy/study/jvm/str/StringTest.java
@@ -0,0 +1,61 @@
+package com.andy.study.jvm.str;
+
+/**
+ * @TODO: javastudy
+ * @author: xiangdan/xiangdan@dtxytech.com
+ * @time: 2020/10/4 16:25  星期日
+ */
+public class StringTest {
+
+
+    public static void main(String[] args) {
+
+        test1();
+
+    }
+
+    public static void test(){
+        System.out.println();
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+        System.out.println("7");
+        System.out.println("8");
+        System.out.println("9");
+        System.out.println("10");
+
+        System.out.println();
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+        System.out.println("7");
+        System.out.println("8");
+        System.out.println("9");
+        System.out.println("10");
+    }
+    public static void test1(){
+        String s = new String("1");
+        s = s.intern();
+        String s2 = "1";
+        System.out.println(s==s2);
+
+        String s3 = new String("2")+new String("3");
+
+
+        System.out.println(s3.hashCode());
+        String s6 = s3.intern();
+        System.out.println(s3.hashCode());
+        System.out.println(s6.hashCode());
+        String s4 = "23";
+        System.out.println(s4.hashCode());
+
+        System.out.println(s3==s4);
+
+
+    }
+
+}
