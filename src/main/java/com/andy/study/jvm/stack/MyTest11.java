5c3100f9b087b65d833ab83a63087aa2a8192ba4
andyluna <xiangdan311@163.com>
2020-10-26 09:22:35 +0800

    classloader

---
+++ src/main/java/com/andy/study/jvm/stack/MyTest11.java
@@ -0,0 +1,13 @@
+package com.andy.study.jvm.stack;
+
+/**
+ * @time: 2020/10/24 十月 22:57
+ * @author: xiangdan/xiangdan@dtxytech.com
+ */
+public class MyTest11 {
+    public static void main(String[] args) {
+        while (true){
+            System.out.println("Hello World");
+        }
+    }
+}
---
+++ src/main/java/com/andy/study/jvm/stack/StackTest2.java
@@ -0,0 +1,32 @@
+package com.andy.study.jvm.stack;
+
+/**
+ * @time: 2020/10/24 十月 22:18
+ * @author: xiangdan/xiangdan@dtxytech.com
+ */
+public class StackTest2 {
+
+    private int length = 0;
+
+    public void test1() {
+        length++;
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        test1();
+    }
+
+
+    public static void main(String[] args) {
+        StackTest2 st = new StackTest2();
+        try {
+            st.test1();
+        }catch (Throwable e){
+            e.printStackTrace();
+            System.out.println(st.length);
+        }
+    }
+
+}
