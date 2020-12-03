cb6245b407715d7c6d98aae8f375e51afee85f2f
andyluna <xiangdan311@163.com>
2020-12-03 16:47:34 +0800

    JSON测试

--- json-study/src/main/java/com/andy/study/json/fasfjson/FastJsonTest01.java
+++ json-study/src/main/java/com/andy/study/json/fasfjson/FastJsonTest01.java
@@ -3,12 +3,16 @@ package com.andy.study.json.fasfjson;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.serializer.SimpleDateFormatSerializer;
 import com.andy.study.json.entity.Person;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Date;
 
 /**
  * @time: 2020/12/3 十二月 14:13
@@ -46,4 +50,22 @@ public class FastJsonTest01 {
         Person person = JSON.parseObject(s, Person.class);
         log.debug("p ={} \n{}",person.getClass(),person);
     }
+
+    @Test
+    public void test04(){
+        SerializeConfig config = new SerializeConfig();
+        SimpleDateFormatSerializer serializer = new SimpleDateFormatSerializer("yyyy-MM-dd HH:mm:ss");
+        config.put(Date.class, serializer);
+
+        String s = JSON.toJSONString(person,config,
+                SerializerFeature.PrettyFormat,
+                SerializerFeature.WriteDateUseDateFormat);
+        log.debug("s = \n{}",s);
+
+        Person person = JSON.parseObject(s, Person.class);
+        log.debug("p ={} \n{}",person.getClass(),person);
+    }
+
+
+
 }
