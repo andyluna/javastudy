329d9ddce61c779cccb7047bcfb4aeb4f0efbc73
andyluna <xiangdan311@163.com>
2021-03-30 23:09:06 +0800

    李逸的代码测试

---
+++ liyi-study/src/main/java/com/liyi/study/study001/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.liyi.study.study001;
+
+/**
+ * @TODO: javastudy
+ * @author: xiangdan/xiangdan@dtxytech.com
+ * @time: 2021/3/30 23:08  星期二
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+
+}
