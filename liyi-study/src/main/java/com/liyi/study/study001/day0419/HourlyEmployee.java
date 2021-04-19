06eadaae4ef2557988b57a408f7c90989fb16063
andyluna <xiangdan311@163.com>
2021-04-19 23:02:04 +0800

    李逸的代码测试

---
+++ liyi-study/src/main/java/com/liyi/study/study001/day0419/HourlyEmployee.java
@@ -0,0 +1,31 @@
+package com.liyi.study.study001.day0419;
+
+/**
+ * @TODO: javastudy
+ * @author: xiangdan/xiangdan@dtxytech.com
+ * @time: 2021/4/19 16:38  星期一
+ */
+public class HourlyEmployee extends Employee{
+
+    private int wage;//每小时的工资
+    private int hour;//月工作的小时数
+
+    public HourlyEmployee(String name, int number, MyDate birthday) {
+        super(name, number, birthday);
+    }
+
+    public HourlyEmployee(String name, int number, MyDate birthday,int wage,int hour) {
+        super(name, number, birthday);
+        this.wage=wage;
+        this.hour=hour;
+    }
+
+    @Override
+    public double earnings() {
+        return wage*hour;
+    }
+
+    public String toString(){
+        return "HourlyEmployee["+super.toString()+"]";
+    }
+}
---
+++ liyi-study/src/main/java/com/liyi/study/study001/day0419/PayrollSystem.java
@@ -0,0 +1,35 @@
+package com.liyi.study.study001.day0419;
+
+import java.util.Scanner;
+
+/**
+ * @TODO: javastudy
+ * @author: xiangdan/xiangdan@dtxytech.com
+ * @time: 2021/4/19 16:44  星期一
+ */
+public class PayrollSystem {
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("请输入当月的月份：");
+        int month=scanner.nextInt();
+
+        Employee[] emps=new Employee[2];
+
+        emps[0]=new SalariedEmployee("马森",1001,new MyDate(1992,2,28),10000);
+
+        emps[1]=new HourlyEmployee("小马",1002,new MyDate(1998,5,6),60,240);
+
+        for (int i=0;i<emps.length;i++){
+            System.out.println(emps[i]);
+            double salary=emps[i].earnings();
+            System.out.println("月工资为："+salary);
+
+            if(month==emps[i].getBirthday().getMonth()){
+                System.out.println("生日快乐，奖励100元");
+            }
+        }
+
+
+    }
+}
