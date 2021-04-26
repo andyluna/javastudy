6b75e2c363d9141ede15e17db1313d7fe73f67a1
andyluna <xiangdan311@163.com>
2021-04-26 23:17:52 +0800

    李逸的代码测试

---
+++ liyi-study/src/main/java/com/liyi/study/study001/day0426/java/Person.java
@@ -0,0 +1,44 @@
+package com.liyi.study.study001.day0426.java;
+
+/**
+ * @TODO: javastudy
+ * @author: xiangdan/xiangdan@dtxytech.com
+ * @time: 2021/4/26 21:23  星期一
+ */
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
+
