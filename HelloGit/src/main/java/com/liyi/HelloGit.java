de261a83fd7b6e4214e4e0fc642298887b850dac
andyluna <xiangdan311@163.com>
2021-05-13 21:05:11 +0800

    李逸的代码测试

---
+++ HelloGit/src/main/java/com/liyi/HelloGit.java
@@ -0,0 +1,14 @@
+package com.liyi;
+
+/**
+ * @TODO: javastudy
+ * @author: xiangdan/xiangdan@dtxytech.com
+ * @time: 2021/5/13 21:01  星期四
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git！");
+    }
+
+
+}
