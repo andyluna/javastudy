6a675f84cb2a71004ddb11d37c681724fd88fd6e
andyluna <xiangdan311@163.com>
2021-06-22 00:54:26 +0800

    新增CommonDao

--- liyi-web/cms/src/main/java/com/study/cms/comm/dao/impl/CommonDaoJdbcImpl.java
+++ liyi-web/cms/src/main/java/com/study/cms/comm/dao/impl/CommonDaoJdbcImpl.java
@@ -6,9 +6,16 @@ import com.study.cms.comm.vo.PageRes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 最原始的方式实现
@@ -18,6 +25,36 @@ import java.util.List;
  */
 public class CommonDaoJdbcImpl implements CommonDao {
     public static final Logger log = LoggerFactory.getLogger(CommonDaoJdbcImpl.class);
+    private static Map<Class<?>,Class<?>> map = null;
+    static{
+        map = new HashMap<>();
+        map.put(boolean.class,Boolean.class);
+        map.put(char.class,Character.class);
+        map.put(byte.class,Byte.class);
+        map.put(short.class,Short.class);
+        map.put(int.class,Integer.class);
+        map.put(long.class,Long.class);
+        map.put(float.class,Float.class);
+        map.put(double.class,Double.class);
+
+        map.put(Boolean.class,Boolean.class);
+        map.put(Character.class,Character.class);
+        map.put(Byte.class,Byte.class);
+        map.put(Short.class,Short.class);
+        map.put(Integer.class,Integer.class);
+        map.put(Long.class,Long.class);
+        map.put(Float.class,Float.class);
+        map.put(Double.class,Double.class);
+
+        map.put(String.class,String.class);
+        map.put(Date.class,Date.class);
+        map.put(java.sql.Date.class,java.sql.Date.class);
+        map.put(java.sql.Timestamp.class,java.sql.Timestamp.class);
+
+
+
+    }
+
     @Override
     public int update(String sql, Object... param) {
         Connection conn = null;
@@ -57,6 +94,78 @@ public class CommonDaoJdbcImpl implements CommonDao {
 
     @Override
     public <T> T selectOne(String sql, Class<T> returnType, Object... param) {
-        return null;
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        Object res = null;
+        try{
+            //1.获取连接
+            conn = JDBCUtils.getCon();
+            //2.创建执行器
+            pstmt = conn.prepareStatement(sql);
+            //3.设置参数
+            if(param!=null && param.length>0){
+                for (int i = 1; i <=param.length ; i++) {
+                    pstmt.setObject(i,param[i-1]);
+                }
+            }
+            //4.执行查询
+            rs = pstmt.executeQuery();
+            Class<?> aClass = map.get(returnType);
+            if(rs.next()){
+
+                if(map.containsKey(returnType)){
+                   if(aClass==Boolean.class){
+                       res = rs.getBoolean(1);
+                   }else if(aClass==Character.class){
+                       String tmp= rs.getString(1);
+                        res = tmp==null?null:tmp.charAt(00);
+                   }else if(aClass==Byte.class){
+                       //res = rs.getInt(1);
+                   }else if(aClass==Short.class){
+                       res = rs.getShort(1);
+                   }else if(aClass==Integer.class){
+                       res = rs.getInt(1);
+                   }else if(aClass==Long.class){
+                       res = rs.getLong(1);
+                   }else if(aClass==Float.class){
+                       res = rs.getFloat(1);
+                   }else if(aClass==Double.class){
+                       res = rs.getDouble(1);
+                   }else if(aClass==String.class){
+                       res = rs.getString(1);
+                   }else if(aClass==Date.class){
+                       res = rs.getDate(1);
+                   }else if(aClass==java.sql.Date.class){
+                       res = rs.getDate(1);
+                   }else if(aClass== Timestamp.class){
+                       res = rs.getTimestamp(1);
+                   }
+
+                }else{//是一个具体的java类
+                    res = returnType.newInstance();
+
+                    Field[] fields = returnType.getDeclaredFields();
+                    for(Field f :fields){
+                        String columnName = f.getName();
+                        Class<?> type = f.getType();
+                        Object value = rs.getObject(columnName,type);
+                        String methodName ="set"+columnName.substring(0,1).toUpperCase()+columnName.substring(1);//得到一个属性对应的set 方法名
+                        Method m = returnType.getMethod(methodName,type);
+                        m.invoke(res,value);
+                    }
+
+
+                }
+            }else{//没有查询到数据
+                log.debug("暂时不考虑");
+            }
+            log.debug("执行selectOne 成功");
+        }catch (Exception e){
+            log.error("执行selectOne 方法出错了",e);
+        }finally {
+            JDBCUtils.close(conn,pstmt,rs);
+        }
+        return (T) res;
     }
 }
--- liyi-web/cms/src/test/java/com/study/cms/comm/dao/impl/CommonDaoJdbcImplTest.java
+++ liyi-web/cms/src/test/java/com/study/cms/comm/dao/impl/CommonDaoJdbcImplTest.java
@@ -1,6 +1,7 @@
 package com.study.cms.comm.dao.impl;
 
 import com.study.cms.comm.dao.CommonDao;
+import com.study.cms.manager.entity.User;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -29,6 +30,63 @@ public class CommonDaoJdbcImplTest {
         String sql = "update c_menu set name =? ,url = ? , open_type=?,last_update_date=? where id = ?";
         commonDao.update(sql,"菜单1","http://www.baidu.com","当前页",new Date(),2);
 
+    }
+
+
+    @Test
+    public void selectOne() {
+        String sql = "select id,username,password,name,phone,address,sex,dept_id from c_user where id = ?";
+        User user = commonDao.selectOne(sql, User.class, 2);
+        System.out.println(user);
+
+
+        String sql1 = "select username from c_user where id = ?";
+        String username = commonDao.selectOne(sql1, String.class, 2);
+        System.out.println(username);
+
+
+        String sql2 = "select create_date from c_dept where id = ?";
+        Date date = commonDao.selectOne(sql2, Date.class, 1);
+        System.out.println(date);
+
+
+        String sql3 = "select count(*)  from c_dept  ";
+        Integer total = commonDao.selectOne(sql3, Integer.class);
+        System.out.println(total);
+
+    }
+
+    @Test
+    public void test1(){
+        Class stringClass=String.class;
+        System.out.println("String is primitive type："+stringClass.isPrimitive());
+
+        Class booleanClass=Boolean.class;
+        System.out.println("Boolean is primitive type："+booleanClass.isPrimitive()+booleanClass);
+
+        Class booleanType=boolean.class;
+        System.out.println("boolean is primitive type："+booleanType.isPrimitive()+booleanType);
+
+        Class byteType=byte.class;
+        System.out.println("byte is primitive type："+byteType.isPrimitive());
+
+        Class charType=char.class;
+        System.out.println("char is primitive type："+charType.isPrimitive());
+
+        Class shortType=short.class;
+        System.out.println("short is primitive type："+shortType.isPrimitive());
+
+        Class intType=int.class;
+        System.out.println("int is primitive type："+intType.isPrimitive());
+
+        Class longType=long.class;
+        System.out.println("long is primitive type："+longType.isPrimitive());
+
+        Class floatType=float.class;
+        System.out.println("float is primitive type："+floatType.isPrimitive());
+
+        Class doubleType=double.class;
+        System.out.println("double is primitive type："+doubleType.isPrimitive());
     }
 
     @After
