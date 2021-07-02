a61464a6506e95d9af64507cbd1bdc225a1cc20c
andyluna <xiangdan311@163.com>
2021-07-02 16:07:01 +0800

    xiangdan提交

--- liyi-study/liyi-spring/src/test/java/com/andy/test/JdbcUtilTest.java
+++ liyi-study/liyi-spring/src/test/java/com/andy/test/JdbcUtilTest.java
@@ -3,6 +3,7 @@ package com.andy.test;
 import com.liyi.spring.spring5.aop.User;
 import com.liyi.spring.spring5.jdbc.JDBCUtils;
 import com.liyi.spring.spring5.jdbc.JDBCUtils2;
+import com.liyi.spring.spring5.jdbc.JDBCUtils3;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -30,6 +31,24 @@ public class JdbcUtilTest {
         JDBCUtils2 jdbcUtils2 = context.getBean("jDBCUtils2", JDBCUtils2.class);
         System.out.println(jdbcUtils2);
         Connection connection = jdbcUtils2.getConnection();
+        System.out.println(connection);
+    }
+
+    @Test
+    public void test3() throws SQLException {
+        ApplicationContext context=new ClassPathXmlApplicationContext("jdbc/bean-jdbc3.xml");
+        JDBCUtils3 jdbcUtils3 = context.getBean("jDBCUtils3", JDBCUtils3.class);
+        System.out.println(jdbcUtils3);
+        Connection connection = jdbcUtils3.getConnection();
+        System.out.println(connection);
+    }
+
+    @Test
+    public void test3_1() throws SQLException {
+        ApplicationContext context=new ClassPathXmlApplicationContext("jdbc/bean-jdbc3-1.xml");
+        JDBCUtils3 jdbcUtils3 = context.getBean("jDBCUtils3", JDBCUtils3.class);
+        System.out.println(jdbcUtils3);
+        Connection connection = jdbcUtils3.getConnection();
         System.out.println(connection);
     }
 }
